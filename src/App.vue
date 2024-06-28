cb363d0fc45b2ba677101621cf9a70a0f1727355
mityaiii <pericle04@mail.ru>
2024-06-28 22:51:36 +0300

    added landing

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import AboutSection from './widgets/project/project.vue';
+import Project from './widgets/project/project.vue';
 import HeroSection from './widgets/hero-section/hero-section.vue'
+import Footer from './widgets/footer/footer.vue'
 </script>
 
 <template>
   <div class="bg-black">
     <HeroSection />
-    <AboutSection />
+    <Project />
+    <Footer/>
   </div>
 </template>
-
-<style scoped>
-</style>
--- src/widgets/hero-section/hero-section.vue
+++ src/widgets/hero-section/hero-section.vue
@@ -1,24 +1,53 @@
 <template>
-  <div class="w-full h-screen flex flex-col justify-center text-white">
-    <div class="flex space-y-10 h-4/5 flex-col justify-center items-center text-center">
+  <div ref="animatedDiv" class="w-full h-screen flex flex-col justify-center text-white fade-in">
+    <div class="flex h-4/5 flex-col justify-around items-center text-center">
       <div class="space-y-4">
         <h1 class="text-5xl text-white">DishDash</h1>
         <p>Сервис быстрого поиска места<br /> для встречи с друзьями</p>
       </div>
-      <img
-        src="./assets/radar.png"
-        class="h-64 md:h-96"
-      />
+      <img src="./assets/radar.png" class="h-64 md:h-96" />
       <p>Заполните форму,<br /> чтобы протестировать бета-версию</p>
-      <Button size="lg" variant="secondary">Оставить заявку</Button>
+      <a href="https://forms.gle/S2JPfT3kVrbNDGqNA">
+        <Button size="lg" variant="secondary">Оставить заявку</Button>
+      </a>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import Button from '@/components/ui/button/Button.vue';
 
-defineProps();
+const animatedDiv = ref(null);
+
+onMounted(() => {
+  const options = {
+    root: null, // это окно просмотра
+    threshold: 0.1, // процент видимости
+  };
+
+  const callback = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+  if (animatedDiv.value) observer.observe(animatedDiv.value);
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+.fade-in {
+  opacity: 0;
+  transform: translateY(10px);
+  transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;
+}
+
+.fade-in.visible {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
